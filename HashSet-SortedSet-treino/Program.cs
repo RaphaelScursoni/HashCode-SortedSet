7a168af7f741e8ceb615e49130ce53a2ca3d5e89
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-20 09:58:29 -0300

    Criando hash set pra entender instanciacão de elementos

---
+++ HashSet-SortedSet-treino/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet_SortedSet_treino {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+
+            Console.WriteLine(set.Contains("TV");
+
+            foreach(string p in set) {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
